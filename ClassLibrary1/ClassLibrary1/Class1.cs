1779108ac10fde4f46e65ddd549805962aa2b99d
Vadim Litvinenko <133224619+dezlife21@users.noreply.github.com>
2024-06-09 21:55:09 +0300

    Add files via upload

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassLibrary1
+{
+    public class Town
+    {
+        public string Name; public string Country; public string Region; public int
+        Population; public double YearIncome; public double Square; public bool HasPort;
+        public bool HasAirport;
+        public double yearIncomePerInhabitant
+        {
+            get
+            {
+                return GetYearIncomePerInhabitant();
+            }
+        }
+        public double GetYearIncomePerInhabitant()
+        {
+            return YearIncome / Population;
+        }
+    }
+}
