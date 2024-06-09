630604e7d6c33a8cb79b17eb5adff49c2b6e1484
Vadim Litvinenko <133224619+dezlife21@users.noreply.github.com>
2024-06-09 21:52:16 +0300

    Add files via upload

---
+++ ClassLibrary2/ClassLibrary2/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassLibrary2
+{
+    public class TabletOwner
+    {
+        public string Name;
+        public string PhoneNumber;
+        public string Price;
+        public string Email;
+        public DateTime SubscriptionStartDate;
+        public bool IsActive;
+        public int Age;
+        public double GetYearsWithTablet()
+        {
+            return (DateTime.Now - SubscriptionStartDate).TotalDays / 365;
+        }
+    }
+}
---
+++ lab05/lab05/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using ClassLibrary1;
+class Program
+{
+    static void Main()
+    {
+        Town[] arrTowns;
+
+        Console.Write("Введiть кiлькiсть мiст: ");
+        int cntTowns = int.Parse(Console.ReadLine());
+
+        arrTowns = new Town[cntTowns];
+
+        for (int i = 0; i < cntTowns; i++)
+        {
+            Console.Write("Введiть назву мiста: ");
+            string sName = Console.ReadLine();
+
+            Console.Write("Введiть назву краiни: ");
+            string sCountry = Console.ReadLine();
+
+            Console.Write("Введiть назву регiону: ");
+            string sRegion = Console.ReadLine();
+
+            Console.Write("Введiть кiлькiсть населення: ");
+            int sPopulation = int.Parse(Console.ReadLine());
+
+            Console.Write("Введiть рiчний дохiд: ");
+            double sYearIncome = double.Parse(Console.ReadLine());
+
+            Console.Write("Введiть площу, кв. км: ");
+            double sSquare = double.Parse(Console.ReadLine());
+
+            Console.Write("Чи є у мiстi порт? (y-так, n-нi): ");
+            bool hasPort = Console.ReadKey().Key == ConsoleKey.Y;
+            Console.WriteLine();
+
+            Console.Write("Чи є у мiстi аеропорт? (y-так, n-нi): ");
+            bool hasAirport = Console.ReadKey().Key == ConsoleKey.Y;
+            Console.WriteLine();
+
+            arrTowns[i] = new Town
+            {
+                Name = sName,
+                Country = sCountry,
+                Region = sRegion,
+                Population = sPopulation,
+                YearIncome = sYearIncome,
+                Square = sSquare,
+                HasPort = hasPort,
+                HasAirport = hasAirport
+            };
+        }
+
+        foreach (Town t in arrTowns)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Данi про мiсто {t.Name}");
+            Console.WriteLine();
+            Console.WriteLine("Країна: " + t.Country);
+            Console.WriteLine("Регiон: " + t.Region);
+            Console.WriteLine("Кiлькiсть населення: " + t.Population);
+            Console.WriteLine("Рiчний дохiд: " + t.YearIncome.ToString("0.00"));
+            Console.WriteLine("Площа: " + t.Square.ToString("0.000"));
+            Console.WriteLine(t.HasPort ? "У мiстi є порт" : "У мiстi нема порту");
+            Console.WriteLine(t.HasAirport ? "У мiстi є аеропорт" : "У мiстi нема аеропорту");
+            Console.WriteLine();
+            Console.WriteLine("Середнiй рiчний дохiд на одного громадянина: " + t.yearIncomePerInhabitant.ToString("0.00"));
+        }
+
+        Console.ReadKey();
+    }
+}
